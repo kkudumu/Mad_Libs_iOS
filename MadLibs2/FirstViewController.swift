1057f3ca8e89685fea83ff0b9ec2cf80b93ace8f
Kio <kkudumu@gmail.com>
2017-11-07 19:02:46 -0800

    Mad Libs Completed. Needs Some Refactoring...

--- MadLibs2/FirstViewController.swift
+++ MadLibs2/FirstViewController.swift
@@ -8,10 +8,19 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController{
+    
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    var answerLabelText = String("Your Mad Libs Answer will go here...")
+    
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        answerLabel.text = answerLabelText
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,6 +28,7 @@ class FirstViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
 
 }
--- MadLibs2/SecondViewController.swift
+++ MadLibs2/SecondViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    
+    @IBOutlet weak var adjectiveTextField: UITextField!
+    
+    @IBOutlet weak var verbTextField: UITextField!
+    
+    @IBOutlet weak var secondVerbTextField: UITextField!
+    
+    @IBOutlet weak var nounTextField: UITextField!
+    
+    @IBAction func submitButtonPressed(_ sender: Any) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +32,34 @@ class SecondViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    //will take the variables from first view controller and import them, basically
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        var DestViewController : FirstViewController = segue.destination as! FirstViewController
+        
+        DestViewController.answerLabelText = "We are having a perfectly " + adjectiveTextField.text! + " time now. Later we will " + verbTextField.text! + " and " + secondVerbTextField.text! + " in the " + nounTextField.text!
+    }
 
 
 }
+
+
+
+//        let adjectivesObject = UserDefaults.standard.object(forKey: "adjectives")
+//
+//        var adjectives: NSMutableArray
+//
+//        if let tempAdjectives = adjectivesObject as? NSMutableArray {
+//
+//            adjectives = tempAdjectives
+//
+//            adjectives.addObjects(from: [adjectiveTextField.text!])
+//
+//        } else {
+//
+//            adjectives = [adjectiveTextField.text!]
+//
+//        }
+//        UserDefaults.standard.set(adjectives, forKey: "adjectives")
+
 
